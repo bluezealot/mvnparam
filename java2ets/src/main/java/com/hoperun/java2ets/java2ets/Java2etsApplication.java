9771c572f4a93437d1a82181501478b70a95822d
bluezealot <qin_xizhou@hoperun.com>
2022-04-20 23:23:52 +0800

    Delete useless code.

--- java2ets/src/main/java/com/hoperun/java2ets/java2ets/Java2etsApplication.java
+++ java2ets/src/main/java/com/hoperun/java2ets/java2ets/Java2etsApplication.java
@@ -3,9 +3,6 @@ package com.hoperun.java2ets.java2ets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,13 +25,6 @@ public class Java2etsApplication {
 	// 	jedisConFactory.setPort(6379);
 	// 	return jedisConFactory;
 	// }
-
-	@Bean(name="redisTemplate")
-	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-		RedisTemplate<String, Object> template = new RedisTemplate<>();
-		template.setConnectionFactory(factory);
-		return template;
-	}
 }
 
 
